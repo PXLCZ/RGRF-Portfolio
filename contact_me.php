df5e0c1508968f68d9905ecc0b37006ebfe82463
Eaton Wong <E10@Eaton-aka-E.local>
2018-12-09 22:59:28 -1000

    E10 RGRF Portfolio

---
+++ contact_me.php
@@ -0,0 +1,30 @@
+<?php 
+// check if fields passed are empty 
+ 
+ if(empty($_POST['name'])   ||    
+    empty($_POST['email'])  ||
+    empty($_POST['phone'])  ||
+    empty($_POST['message'])||   
+    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))    
+  {     
+       echo "No arguments Provided!";   return false;    
+  } 
+     
+  $name = $_POST['name']; 
+  $email_address = $_POST['email']; 
+  $phone = $_POST['phone']; 
+  $message = $_POST['message'];      
+ 
+ // create email body and send it    
+ $to = 'wonge05@gmail.com'; 
+ // put your email 
+ $email_subject = "Contact form submitted by:  $name"; $email_body = "You have received a new message. \n\n".                 
+                   " Here are the details:\n \n Name: $name \n ".
+                   " Phone: $phone \n ".
+                   " Email: $email_address \n ".
+                   " Here is the message: $message";                   
+ $headers = "From: contacts@realgoodrealfast.com\n"; 
+ $headers .= "Reply-To: $email_address";     
+ 
+ mail($to,$email_subject,$email_body,$headers); return true;             
+?>
